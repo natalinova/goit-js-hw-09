a249896af6e37bcedbdbf508bf3d9752687aa8d5
Nataliya Novak <novak.natali@gmail.com>
2022-08-16 00:08:59 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,63 @@
 import Notiflix from 'notiflix';
+const form = document.querySelector('.form')
 const promiseDelay = document.querySelector('[name="delay"]');
 const promiseStep = document.querySelector('[name="step"]');
 const promiseAmount = document.querySelector('[name="amount"]');
 const buttonStart = document.querySelector('button')
+let startPosition = null;
+let delay = null;
+let amount = null;
 
-
+form.addEventListener('input', onFormInput)
 buttonStart.addEventListener('click', onStartClick);
+
+function onFormInput() {
+  onDelayInputValue();
+  onStepInputValue();
+  onAmountInputValue();
+}
+function onDelayInputValue() {
+  startPosition = Number(promiseDelay.value);
+  if (promiseDelay.value === '') {
+    startPosition = 0;
+  }
+  console.log(startPosition)
+}
+function onStepInputValue() {
+  delay = Number(promiseStep.value);
+  if (promiseStep.value === '') {
+    delay = 0;
+  }
+  console.log(delay)
+}
+function onAmountInputValue() {
+  amount = Number(promiseAmount.value);
+  if (promiseAmount.value === '') {
+    amount = 0;
+    
+  }
+  console.log(amount)
+}
 function onStartClick(e) {
   e.PreventDefault();
-  const startPosition = promiseDelay.value;
+  // const startPosition = Number(promiseDelay.value);
   // console.log(startPosition)
-  const step = promiseStep.value;
-  console.log(step)
-  const amount = promiseAmount.value;
-  console.log(amount)
-  let delay = 0;
+  // const step = Number(promiseStep.value);
+  // console.log(step)
+  // const amount = Number(promiseAmount.value);
+  // console.log(amount)
+
   for (i = 1; i <= amount; i ++){
     const position = i;
     // console.log(position)
     if (i === 1) {
       delay += startPosition;
+      console.log(delay)
     }
     else {
       delay += step;
+      console.log(delay)
+      
     }
 
     
@@ -54,5 +89,5 @@ function createPromise(position, delay) {
     },delay )
   }
   )
-  return promise
+  // return promise
 }
